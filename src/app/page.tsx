f9042d17b4be960e916628f78f076ebccf414057
elitonsouzadev <elitondesenvolvedor@gmail.com>
2024-01-06 15:19:36 -0300

    alinhamento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <div className='h-full w-full bg-gray-200 flex flex-col items-center p-4'>
-        <h5 className="mt-4 mb-4 text-3xl font-semibold text-gray-800">GRS Quadros e Sublimação</h5>
+        <h5 className="mt-4 mb-4 text-3xl font-semibold text-gray-800 text-center">GRS Quadros e Sublimação</h5>
         <CardHome
           texto="Desenvolvimento de Artes"
           subtexto="Da concepção à materialização, nossa equipe dedicada de designers transforma suas ideias em obras de arte exclusivas, alinhadas à identidade única da sua marca. Cada criação é pensada para transmitir a essência e valores que tornam sua marca memorável."
