624662781a3eacafcc7b3cbba1102f50e1ed2421
elitonsouzadev <elitondesenvolvedor@gmail.com>
2024-01-06 15:26:21 -0300

    corrigido mapa bugado do jean

--- src/app/components/map_location/index.tsx
+++ src/app/components/map_location/index.tsx
@@ -1,9 +1,9 @@
 function MapLocation() {
-  return <div className='flex items-center justify-center mt-10 md:mt-0 md:mb-10 px-4'>
+  return <div className='flex items-center justify-center mt-10 md:mt-0 md:mb-10 px-4 w-full'>
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7117.031611569855!2d-49.23720719402449!3d-26.887120534391116!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94df0283c08f19f7%3A0x3894cb04bcc292b1!2sGRS%20Quadros!5e0!3m2!1spt-BR!2sbr!4v1695858859065!5m2!1spt-BR!2sbr"
-      width='1200'
-      className='h-60 md:h-96'
+      width='100vw'
+      className='h-60 md:h-96 w-full'
       loading='lazy'
     ></iframe>
   </div>
